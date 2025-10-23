c57c9964cb01bcc61a18d904f7af02d9d9dab5bd
Ba9A-tata <yogadzakkii@gmail.com>
2025-10-23 12:39:58 +0700

    melengkapi sebagian kode, dan membenarkan typo code

--- app/src/main/java/com/example/activity3_composablelayout2/MainActivity.kt
+++ app/src/main/java/com/example/activity3_composablelayout2/MainActivity.kt
@@ -1,25 +1,72 @@
 package com.example.activity3_composablelayout2
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.activity3_composablelayout2.ui.theme.Activity3ComposableLayout2Theme
+import androidx.compose.ui.res.stringResource
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.AlertDialogDefaults.containerColor
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 
-fun ActivitasPetama(modifier: Modifier){
+fun ActivitasPertama(modifier: Modifier){
     Column(modifier = Modifier.padding(top=100.dp)
-    fillMaxSize(),
-        horizontalAligment = Aligment.CenterHorizontally
+        .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
     ){
-
+        Text( stringResource( R.string.prodi),
+        fontSize = 35.sp,
+        fontWeight = FontWeight.Bold)
+        Text( stringResource( R.string.univ),
+            fontSize = 22.sp)
+        Spacer(modifier = Modifier.height(25.dp))
+        Card (modifier = Modifier
+            .fillMaxWidth(1f)
+            .padding(12.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = Color.DarkGray
+            )) {
+            Row(){
+                val gambar = painterResource(R.drawable.logo_umy)
+                Image(
+                    painter = gambar,
+                    contentDescription = null,
+                    modifier = modifier.size(100.dp).padding(5.dp)
+                )
+                Spacer(modifier = modifier.width(30.dp))
+                Column(){
+                    Text(
+                        stringResource(R.string.nama),
+                        fontSize = 30.sp,
+                        fontFamily = FontFamily.Cursive,
+                        color = Color.White,
+                        modifier = Modifier.padding(top=15.dp)
+                    )
+                    Text(
+                        stringResource(R.string.alamat),
+                        fontSize = 20.sp,
+                    )
+                }
+            }
+        }
     }
 }
