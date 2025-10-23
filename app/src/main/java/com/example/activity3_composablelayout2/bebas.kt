f19570cc33c5b31342bb1319a1711e877b638d7e
Ba9A-tata <yogadzakkii@gmail.com>
2025-10-23 13:33:50 +0700

    membuat tulisan alamat menjadi berwarna kuning

--- app/src/main/java/com/example/activity3_composablelayout2/bebas.kt
+++ app/src/main/java/com/example/activity3_composablelayout2/bebas.kt
@@ -62,7 +62,7 @@ fun ActivitasPertama(modifier: Modifier){
                     Text(
                         stringResource(R.string.alamat),
                         fontSize = 20.sp,
-                        
+                        color = Color.Yellow
                     )
 
                 }
