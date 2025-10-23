9a4c05ed417e66df0ddff35c2b7a07a48da5c74f
Ba9A-tata <yogadzakkii@gmail.com>
2025-10-23 11:22:50 +0700

    membuat fungsi actvitas pertama

--- app/src/main/java/com/example/activity3_composablelayout2/MainActivity.kt
+++ app/src/main/java/com/example/activity3_composablelayout2/MainActivity.kt
@@ -13,35 +13,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.activity3_composablelayout2.ui.theme.Activity3ComposableLayout2Theme
 
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            Activity3ComposableLayout2Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
-
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Activity3ComposableLayout2Theme {
-        Greeting("Android")
-    }
+fun ActivitasPetama(modifier: Modifier){
+
 }
